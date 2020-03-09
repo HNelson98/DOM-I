9f45951071357db77971acd7703814ceb5e3d674
Henry Nelson <Henry.Nelson89@gmail.com>
2020-03-09 15:42:48 -0700

    all done!

--- js/index.js
+++ js/index.js
@@ -44,17 +44,49 @@ logo.setAttribute('src', siteContent["nav"]["img-src"])
 const links = document.getElementsByTagName('a');
 
 Array.from(links);
-  links[0].textContent = "Services";
-  links[1].textContent = "Products";
-  links[2].textContent = "Vision";
-  links[3].textContent = "Features";
-  links[4].textContent = "About";
-  links[5].textContent = "Contact";
+  links[0].textContent = siteContent['nav']['nav-item-1'];
+  links[1].textContent = siteContent['nav']['nav-item-2'];
+  links[2].textContent = siteContent['nav']['nav-item-3'];
+  links[3].textContent = siteContent['nav']['nav-item-4'];
+  links[4].textContent = siteContent['nav']['nav-item-5'];
+  links[5].textContent = siteContent['nav']['nav-item-6'];
 
 document.querySelector('h1').textContent = "DOM Is Awesome";
 
-document.querySelector('button').textContent = "Get Started"
+document.querySelector('button').textContent = siteContent['cta']['button'];
 const ctaImg=
 document.querySelector('#cta-img');
 
-ctaImg.src = "img/header-img.png";
+ctaImg.src = siteContent['cta']['img-src'];
+
+const mainContentH4 = document.querySelectorAll('h4');
+
+mainContentH4[0].textContent = siteContent ['main-content']['features-h4'];
+mainContentH4[1].textContent = siteContent ['main-content']['about-h4'];
+mainContentH4[2].textContent = siteContent ['main-content']['services-h4'];
+mainContentH4[3].textContent = siteContent ['main-content']['product-h4'];
+mainContentH4[4].textContent = siteContent ['main-content']['vision-h4'];
+
+
+const mainContentP = document.querySelectorAll('p');
+
+mainContentP[0].textContent = siteContent ['main-content']['features-content'];
+mainContentP[1].textContent = siteContent ['main-content']['about-content'];
+mainContentP[2].textContent = siteContent ['main-content']['services-content'];
+mainContentP[3].textContent = siteContent ['main-content']['product-content'];
+mainContentP[4].textContent = siteContent ['main-content']['vision-content'];
+
+
+document.querySelector('#middle-img').src = siteContent ['main-content']["middle-img-src"]
+
+//contact
+mainContentH4[5].textContent = siteContent ['contact']['contact-h4']
+
+mainContentP[5].textContent = siteContent ['contact']['address']
+mainContentP[6].textContent = siteContent ['contact']['phone']
+mainContentP[7].textContent = siteContent ['contact']['email']
+
+
+//footer
+
+mainContentP[8].textContent = siteContent ['footer']['copyright']
